485b71412e496a19d67cd74a95cb0a0399045ba9
Fedor M <muhinfa@gmail.com>
2022-05-24 20:13:42 +0300

    solving task 4

---
+++ 004/script.js
@@ -0,0 +1,12 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+"use strict";
+
+const opposite = (number) => -number;
+console.log(opposite(9));
